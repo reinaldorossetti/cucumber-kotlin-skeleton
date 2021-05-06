ceb077fe54491eead7fd290ee3bd9f3a96219da0
reinaldorossetti <reinaldo.rossetti@outlook.com>
2021-05-06 12:12:06 -0300

    teste local

--- src/test/kotlin/qa/example/tests/LoginTest.kt
+++ src/test/kotlin/qa/example/tests/LoginTest.kt
@@ -1,46 +1,21 @@
 package qa.example.tests
 
+import LocalTestBase
 import io.qameta.allure.Allure.step
 import io.qameta.allure.Step
 import io.qameta.allure.model.Status
-import org.junit.jupiter.api.DisplayName
-import org.junit.jupiter.api.Test
-import org.junit.jupiter.api.BeforeAll
+import kotlin.test.Test
+import kotlin.test.assertEquals
 import qa.example.pages.login.GoogleFind
-import org.openqa.selenium.WebDriver
-import org.openqa.selenium.chrome.ChromeDriver
-import java.net.URI
-import java.util.concurrent.TimeUnit
 
-class AllureSimpleTest() {
 
-    lateinit var driver: WebDriver
-        private set
+class AllureSimpleTest(): LocalTestBase() {
 
-//    companion object {
-//        @BeforeAll
-//        @JvmStatic
-//        internal fun beforeAll() {
-//            println("beforeAll called")
-//        }
-//        internal fun setup() {
-////        System.setProperty(UtilResources.getProperties("nameDriver"),
-////            UtilResources.getProperties("pathDriver") + UtilResources.getProperties("exeDriver"))
-//            driver = ChromeDriver()
-//            driver!!.manage()?.timeouts()?.implicitlyWait(10, TimeUnit.SECONDS)
-//            driver!!.manage()?.window()?.maximize()
-//            driver!!.get("https://www.google.com/")
-//        }
-//    }
+    //private lateinit var driver: WebDriver
 
     @Test
-    @DisplayName("allureSimpleTest displayName")
     fun allureSimpleTest() {
         step("Simple step")
-        driver = ChromeDriver()
-        driver.manage()?.timeouts()?.implicitlyWait(10, TimeUnit.SECONDS)
-        driver.manage()?.window()?.maximize()
-        driver.get("https://www.google.com/")
         val google = GoogleFind(driver)
         google.pesquisaGoogle()
         step("Simple step with status", Status.FAILED)
--- src/test/kotlin/qa/example/tests/TestBase.kt
+++ src/test/kotlin/qa/example/tests/TestBase.kt
@@ -1,27 +1,25 @@
-//import org.openqa.selenium.WebDriver
-//import org.openqa.selenium.chrome.ChromeDriver
-//import org.testng.annotations.AfterTest
-//import org.testng.annotations.BeforeTest
-//import java.net.URI
-//import java.util.concurrent.TimeUnit
-//
-//abstract class TestBase {
-//
-//    lateinit var driver: WebDriver
-//        private set
-//
-//    @BeforeTest
-//    fun setup() {
-//        System.setProperty(UtilResources.getProperties("nameDriver"),
-//            UtilResources.getProperties("pathDriver") + UtilResources.getProperties("exeDriver"))
-//        driver = ChromeDriver()
-//        driver?.manage()?.timeouts()?.implicitlyWait(10, TimeUnit.SECONDS)
-//        driver?.manage()?.window()?.maximize()
-//        driver?.get(URI(UtilResources.getProperties("pageURL")).toString())
-//    }
-//
-//    @AfterTest
-//    fun driverClose() {
-//        driver?.close();
-//    }
-//}
+import org.openqa.selenium.WebDriver
+import org.openqa.selenium.chrome.ChromeDriver
+import org.junit.After
+import org.junit.Before
+import java.util.concurrent.TimeUnit
+
+open class LocalTestBase {
+
+    lateinit var driver: WebDriver
+
+    @Before
+    fun setup() {
+        // Before each test
+        driver = ChromeDriver()
+        driver.manage()?.timeouts()?.implicitlyWait(30, TimeUnit.SECONDS)
+        driver.manage()?.window()?.maximize()
+        driver.get("https://www.google.com/")
+    }
+
+    @After
+    fun teardown() {
+        // After each test
+        driver.close()
+    }
+}
